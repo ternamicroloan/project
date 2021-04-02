3312c41658dd996c1c3eb86783af785db2b5e4f6
aditya agarkhed <iamhardwell100@gmail.com>
2021-04-02 22:40:16 +0530

    Preparing to deploy

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,16 @@ app.use('/upload',uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
-app.get('/',(req,res)=>{
-    res.send('API is running...')
-})
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+  app.get('/',(req,res)=>{
+    res.send('API is running...') 
+  })
+}
+
 
 const PORT = process.env.PORT || 5000
 
